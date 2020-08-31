018fffa3a18dd25ad97132efa49e81dbd104b880
armandojsalazar <armando.salazar89@gmail.com>
2020-08-31 10:18:14 -0500

    Coffee Pair Project

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict"
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light', all: 'all'},
     {id: 2, name: 'Half City', roast: 'light', all: 'all'},
@@ -36,7 +37,20 @@ function renderCoffee(coffee) {
     var html = '<div class="coffee">';
     // html += '<h1>' + coffee.id + '</h1>';
     html += '<h3>' + coffee.name + '</h3>';
-    html += '<p>' + coffee.roast + '</p>';
+
+    if(coffee.roast === "light"){
+      html += '<p class="light">' + coffee.roast + '</p>';
+        console.log("Light: ")
+    }
+    else if(coffee.roast === "medium"){
+      html += '<p class="medium">' + coffee.roast + '</p>';
+        console.log("Medium: ")
+    }
+    else{
+      html += '<p class="dark">' + coffee.roast + '</p>';
+      console.log("Dark: ")
+    }
+
     html += '</div>';
 
     return html;
@@ -45,7 +59,7 @@ function renderCoffee(coffee) {
 //Loops thru list to wrap HTML tags
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = 0; i <= coffees.length - 1; i++) {
+    for (var i = 0; i <= coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html; // filters the list
@@ -59,11 +73,10 @@ function updateCoffees() {
     var selectedAll = all.value;
     var filteredCoffees = [];
 
-    coffees.forEach(function(coffee) {
+    coffees.forEach(function (coffee) {
         if (coffee.all === selectedAll && coffee.name.toLowerCase().includes(selectedCoffee.toLowerCase())) {
             filteredCoffees.push(coffee)
-        }
-        else if (coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(selectedCoffee.toLowerCase())) {
+        } else if (coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(selectedCoffee.toLowerCase())) {
             filteredCoffees.push(coffee);
         }
     });
@@ -71,7 +84,7 @@ function updateCoffees() {
 }
 
 //Function for Adding Coffee
-function createCoffee (inputNewCoffee, inputRoast) {
+function createCoffee(inputNewCoffee, inputRoast) {
     var addingCoffeeName = addingCoffee.value;
     var roastAdd = addingRoast.value;
     var addButton = addSubmit.value;
@@ -79,23 +92,22 @@ function createCoffee (inputNewCoffee, inputRoast) {
     var addCoffee = {id: coffees.length + 1, name: addingCoffeeName, roast: roastAdd, all: 'all'};
     if (addingCoffeeName === "") {
         updateCoffees();
-    }
-    else {
+    } else {
         coffees.push(addCoffee);
     }
 }
 
 //Event Handlers
-submitButton.addEventListener('click', function(e) {
+submitButton.addEventListener('click', function (e) {
     e.preventDefault()
     updateCoffees();
 });
 
-search.addEventListener('keyup', function(){
+search.addEventListener('keyup', function () {
     updateCoffees();
 });
 
-addSubmit.addEventListener('click' ,function(e) {
+addSubmit.addEventListener('click', function (e) {
     e.preventDefault()
     createCoffee();
     updateCoffees();
